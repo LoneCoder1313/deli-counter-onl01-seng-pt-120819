22a8cb4f3f8ce5789adf142fb3bd1ab4a5b53cc9
Ricky dildy <rdildy92@gmail.com>
2019-12-22 17:47:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,10 @@
 # Write your code here.
 katz_deli = []
 def take_a_number(katz_deli, name)
-  
-  
+     katz_deli.push(name)
+     position = katz_deli.index(name)
+     puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line "
+end   
   
   
   
