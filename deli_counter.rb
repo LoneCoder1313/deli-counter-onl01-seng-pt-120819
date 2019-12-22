9b481c6c703733342a2fc89f2d522d523e7116ac
Ricky dildy <rdildy92@gmail.com>
2019-12-22 18:20:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(katz_deli)
     if katz_deli.empty? == false
        katz_deli.each_with_index do|value,index|
 
-    puts "The line is currently: #{index+1}. #{value}"
+    puts "The line is currently: #{index+3}. #{value}"
 end
     elsif katz_deli.empty? == true
   puts "The line is currently empty."
