04fe82eb2cdc10663eedfd8829f1f600d53f8c0d
Ricky dildy <rdildy92@gmail.com>
2019-12-22 18:44:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,8 @@ def take_a_number(katz_deli, name)
 end
 
 def line(katz_deli)
-    if katz_deli.length <= line
+    if katz_deli.empty? == false
+      katz_deli.each_with_index do |name,index|
        puts "The line is currently #{index+1}. #{name}"
 end
     elsif katz_deli.empty? == true
