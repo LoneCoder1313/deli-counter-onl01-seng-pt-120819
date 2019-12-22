7ba96d49847704986791b20e4afca96597ad216a
Ricky dildy <rdildy92@gmail.com>
2019-12-22 18:23:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 def line(katz_deli)
     if katz_deli.empty? == false
-       katz_deli.each_with_index do|value,index|
+       katz_deli.each_with_index do|name,index|
 
-    puts "The line is currently: #{index+1}. #{value}"
+    puts "The line is currently: #{index+1}. #{name}"
 end
     elsif katz_deli.empty? == true
   puts "The line is currently empty."
