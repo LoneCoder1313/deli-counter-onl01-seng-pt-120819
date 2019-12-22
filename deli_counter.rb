ca45eb140034a88020a3454d71f76b2fe2968a14
Ricky dildy <rdildy92@gmail.com>
2019-12-22 17:53:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,13 +7,13 @@ def take_a_number(katz_deli, name)
      return name, position
 end   
   def line(katz_deli)
-if katz_deli.empty? == false
-  katz_deli.each_with_index do|value,index|
+    if katz_deli.empty? == false
+       katz_deli.each_with_index do|value,index|
 
-    puts "The line is currently: #{index+1}. #{value}"
+       puts "The line is currently: #{index+1}. #{value}"
 end
     elsif katz_deli.empty? == true
-  puts "The line is currently empty."
+      puts "The line is currently empty."
 end
 end
   
