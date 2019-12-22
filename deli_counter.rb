d5108f33f61d10c36e21da3c0e147387eaf81fcf
Ricky dildy <rdildy92@gmail.com>
2019-12-22 17:49:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def take_a_number(katz_deli, name)
      katz_deli.push(name)
      position = katz_deli.index(name)
      puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line "
+     return name, position
 end   
   
   
