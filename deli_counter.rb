261304f86ddd9106aa0fca91206a7dc911652bec
Ricky dildy <rdildy92@gmail.com>
2019-12-22 18:20:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,23 +8,3 @@ end
   puts "The line is currently empty."
 end
 end
-  
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  position = katz_deli.index(name)
-  puts"Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
-
-  return name, position
-end
-
-
-
-def now_serving (katz_deli)
-  if katz_deli.empty? == true
-    puts"There is nobody waiting to be served!"
-  elsif katz_deli.empty? == false
-
-    puts"Currently serving #{katz_deli.first}."
-  katz_deli.shift
-  end
-end
